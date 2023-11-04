6c2f284116f8c9abfbe0fd2d4ccd5ab9369ffeda
Mark Vander Stel <mvndrstl@gmail.com>
2023-11-04 17:30:01 -0400

    Add layer to planners
    These icons are from the shortcut bar, which means I need to make less
    icons, and are images users will be used to.

--- data.lua
+++ data.lua
@@ -26,8 +26,26 @@ local function icon_add_letter(name, letter)
   }
 end
 
+local function overlay_icon(_type, item, icon)
+  data.raw[_type][item].icons = {
+    {
+      icon = data.raw[_type][item].icon,
+      icon_size = 64,
+      icon_mipmaps = 4,
+    }, {
+      icon = icon,
+      icon_size = 32,
+      icon_mipmaps = 2,
+    },
+  }
+end
+
 if config("electronic-circuit-custom") then
   override_icon("electronic-circuit")
 elseif config("electronic-circuit-letter") then
   icon_add_letter("electronic-circuit", "A")
 end
+
+overlay_icon("blueprint", "blueprint", "__base__/graphics/icons/shortcut-toolbar/mip/new-blueprint-x32-white.png")
+overlay_icon("deconstruction-item", "deconstruction-planner", "__base__/graphics/icons/shortcut-toolbar/mip/new-deconstruction-planner-x32-white.png")
+overlay_icon("upgrade-item", "upgrade-planner", "__base__/graphics/icons/shortcut-toolbar/mip/new-upgrade-planner-x32-white.png")
