c1c5c12bc7bb3c7579bedf92f721264f84ca041f
Mark Vander Stel <mvndrstl@gmail.com>
2023-11-04 16:25:30 -0400

    Add letter overlay test

--- data.lua
+++ data.lua
@@ -9,6 +9,25 @@ local function override_icon(name)
   data.raw["item"][name].icon = Icons .. name .. ".png"
 end
 
-if config "electronic-circuit" then
-  override_icon "electronic-circuit"
+local function icon_add_letter(name, letter)
+  data.raw["item"][name].icons = {
+    {
+      icon = '__base__/graphics/icons/' .. name .. '.png',
+      icon_size = 64,
+      icon_mipmaps = 4,
+    },
+    {
+      icon = Icons .. letter .. '.png',
+      icon_size = 64,
+      icon_mipmaps = 4,
+      scale = 0.3,
+      shift = {7, 7},
+    },
+  }
+end
+
+if config("electronic-circuit-custom") then
+  override_icon("electronic-circuit")
+elseif config("electronic-circuit-letter") then
+  icon_add_letter("electronic-circuit", "A")
 end
--- settings.lua
+++ settings.lua
@@ -1,11 +1,20 @@
 data:extend({
   {
-    name = "colorblind_ultimate__electronic-circuit",
+    name = "colorblind_ultimate__electronic-circuit-custom",
     type = "bool-setting",
     setting_type = "startup",
     default_value = false,
     order = "100",
-    localised_name = "Electronic circuit",
+    localised_name = "Electronic circuit custom",
+    localised_description = "custom icon",
+  },
+  {
+    name = "colorblind_ultimate__electronic-circuit-letter",
+    type = "bool-setting",
+    setting_type = "startup",
+    default_value = false,
+    order = "100",
+    localised_name = "Electronic circuit letter",
     localised_description = "custom icon",
   },
 })
