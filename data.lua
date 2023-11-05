38591f3ac7b5794cc691b9e13ffd8168d895fb54
Mark Vander Stel <mvndrstl@gmail.com>
2023-11-05 17:35:00 -0500

    Add a bunch more icon overlays

--- data.lua
+++ data.lua
@@ -9,43 +9,150 @@ local function override_icon(name)
   data.raw["item"][name].icon = Icons .. name .. ".png"
 end
 
-local function icon_add_letter(name, letter)
-  data.raw["item"][name].icons = {
+local function overlay_icon(_type, item, icon, icon2)
+  local obj = data.raw[_type][item]
+  obj.icons = {
     {
-      icon = '__base__/graphics/icons/' .. name .. '.png',
-      icon_size = 64,
-      icon_mipmaps = 4,
+      icon = obj.icon,
+      icon_size = obj.icon_size,
+      icon_mipmaps = obj.icon_mipmaps,
     },
-    {
-      icon = Icons .. letter .. '.png',
-      icon_size = 64,
-      icon_mipmaps = 4,
-      scale = 0.3,
-      shift = {7, 7},
-    },
+    icon,
   }
+
+  if icon2 then
+    table.insert(obj.icons, icon2)
+  end
 end
 
-local function overlay_icon(_type, item, icon)
-  data.raw[_type][item].icons = {
-    {
-      icon = data.raw[_type][item].icon,
-      icon_size = 64,
-      icon_mipmaps = 4,
-    }, {
-      icon = icon,
-      icon_size = 32,
-      icon_mipmaps = 2,
-    },
-  }
+local function table_merge(t1, t2)
+  local t = {}
+  for k,v in pairs(t1) do
+    t[k] = v
+  end
+  for k,v in pairs(t2) do
+    t[k] = v
+  end
+  return t
 end
 
 if config("electronic-circuit-custom") then
   override_icon("electronic-circuit")
-elseif config("electronic-circuit-letter") then
-  icon_add_letter("electronic-circuit", "A")
 end
 
-overlay_icon("blueprint", "blueprint", "__base__/graphics/icons/shortcut-toolbar/mip/new-blueprint-x32-white.png")
-overlay_icon("deconstruction-item", "deconstruction-planner", "__base__/graphics/icons/shortcut-toolbar/mip/new-deconstruction-planner-x32-white.png")
-overlay_icon("upgrade-item", "upgrade-planner", "__base__/graphics/icons/shortcut-toolbar/mip/new-upgrade-planner-x32-white.png")
+local mip_overlay = {
+  icon_size = 32,
+  icon_mipmaps = 2,
+}
+
+local tooltip_overlay = {
+  icon_size = 40,
+  icon_mipmaps = 2,
+  scale = 0.4,
+  shift = {-8, 8},
+}
+
+local icon_overlay = {
+  icon_size = 64,
+  icon_mipmaps = 4,
+  scale = 0.25,
+  shift = {-8, 8},
+}
+
+overlay_icon("blueprint", "blueprint", table_merge(mip_overlay, {
+  icon = "__base__/graphics/icons/shortcut-toolbar/mip/new-blueprint-x32-white.png",
+}))
+overlay_icon("deconstruction-item", "deconstruction-planner", table_merge(mip_overlay, {
+  icon = "__base__/graphics/icons/shortcut-toolbar/mip/new-deconstruction-planner-x32-white.png",
+}))
+overlay_icon("upgrade-item", "upgrade-planner", table_merge(mip_overlay, {
+  icon = "__base__/graphics/icons/shortcut-toolbar/mip/new-upgrade-planner-x32-white.png",
+}))
+
+local nuclear_overlay = table_merge(tooltip_overlay, {
+  icon = "__base__/graphics/icons/tooltips/tooltip-category-nuclear.png",
+})
+overlay_icon("item", "nuclear-fuel", nuclear_overlay)
+overlay_icon("item", "uranium-235", nuclear_overlay)
+overlay_icon("item", "uranium-fuel-cell", nuclear_overlay)
+overlay_icon("ammo", "uranium-cannon-shell", nuclear_overlay)
+overlay_icon("ammo", "uranium-rounds-magazine", nuclear_overlay)
+overlay_icon("ammo", "atomic-bomb", nuclear_overlay)
+
+local explosive_overlay = table_merge(icon_overlay, {
+  icon = "__base__/graphics/icons/explosives.png",
+})
+overlay_icon("ammo", "explosive-cannon-shell", explosive_overlay)
+overlay_icon("ammo", "explosive-rocket", explosive_overlay)
+overlay_icon("capsule", "cluster-grenade", explosive_overlay)
+
+local shifted_explosive_overlay = table_merge(explosive_overlay, {
+  shift = {-8, -8},
+})
+overlay_icon("ammo", "explosive-uranium-cannon-shell", nuclear_overlay, shifted_explosive_overlay)
+
+local piercing_overlay = table_merge(icon_overlay, {
+  icon = "__base__/graphics/icons/iron-stick.png",
+})
+overlay_icon("ammo", "piercing-rounds-magazine", piercing_overlay)
+overlay_icon("ammo", "piercing-shotgun-shell", piercing_overlay)
+
+local productivity_overlay = table_merge(tooltip_overlay, {
+  icon = Icons .. "tooltips/gear.png",
+})
+overlay_icon("module", "productivity-module", productivity_overlay)
+overlay_icon("module", "productivity-module-2", productivity_overlay)
+overlay_icon("module", "productivity-module-3", productivity_overlay)
+
+local effectivity_overlay = table_merge(tooltip_overlay, {
+  icon = Icons .. "tooltips/electricity.png",
+})
+overlay_icon("module", "effectivity-module", effectivity_overlay)
+overlay_icon("module", "effectivity-module-2", effectivity_overlay)
+overlay_icon("module", "effectivity-module-3", effectivity_overlay)
+
+local speed_overlay = table_merge(tooltip_overlay, {
+  icon = Icons .. "tooltips/speed.png",
+})
+overlay_icon("module", "speed-module", speed_overlay)
+overlay_icon("module", "speed-module-2", speed_overlay)
+overlay_icon("module", "speed-module-3", speed_overlay)
+
+overlay_icon("item", "logistic-chest-active-provider", table_merge(tooltip_overlay, {
+  icon = Icons .. "tooltips/pushes.png",
+}))
+overlay_icon("item", "logistic-chest-passive-provider", table_merge(tooltip_overlay, {
+  icon = Icons .. "tooltips/generates.png",
+}))
+overlay_icon("item", "logistic-chest-buffer", table_merge(tooltip_overlay, {
+  icon = Icons .. "tooltips/buffer.png",
+}))
+overlay_icon("item", "logistic-chest-requester", table_merge(tooltip_overlay, {
+  icon = Icons .. "tooltips/consumes.png",
+}))
+overlay_icon("item", "logistic-chest-storage", table_merge(tooltip_overlay, {
+  icon = Icons .. "tooltips/storage.png",
+}))
+
+
+overlay_icon("tool", "automation-science-pack", table_merge(icon_overlay, {
+  icon = "__base__/graphics/icons/iron-gear-wheel.png",
+}))
+overlay_icon("tool", "logistic-science-pack", table_merge(icon_overlay, {
+  icon = "__base__/graphics/icons/inserter.png",
+}))
+overlay_icon("tool", "military-science-pack", table_merge(icon_overlay, {
+  icon = "__base__/graphics/icons/piercing-rounds-magazine.png",
+}))
+overlay_icon("tool", "chemical-science-pack", table_merge(icon_overlay, {
+  icon = "__base__/graphics/icons/fluid/sulfuric-acid.png",
+}))
+overlay_icon("tool", "production-science-pack", table_merge(icon_overlay, {
+  icon = "__base__/graphics/icons/productivity-module.png",
+}))
+overlay_icon("tool", "utility-science-pack", table_merge(icon_overlay, {
+  icon = "__base__/graphics/icons/processing-unit.png",
+}))
+overlay_icon("tool", "space-science-pack", table_merge(icon_overlay, {
+  icon = "__base__/graphics/icons/satellite.png",
+}))
--- settings.lua
+++ settings.lua
@@ -8,14 +8,5 @@ data:extend({
     localised_name = "Electronic circuit custom",
     localised_description = "custom icon",
   },
-  {
-    name = "colorblind_ultimate__electronic-circuit-letter",
-    type = "bool-setting",
-    setting_type = "startup",
-    default_value = false,
-    order = "100",
-    localised_name = "Electronic circuit letter",
-    localised_description = "custom icon",
-  },
 })
 
