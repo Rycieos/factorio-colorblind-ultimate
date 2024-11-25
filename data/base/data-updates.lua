57e17e123e9459dbe651722d8ecb0fcccd5cc046
Mark Vander Stel <mvndrstl@gmail.com>
2024-11-25 13:13:17 -0500

    Fix overlay of fluid overlay crashing
    The method used here was stupid anyway. Rather than reload the
    prototypes and reparse the settings, just take the overlay directly from
    the fluid icon. This will handle Space Age and likely other modded
    fluids as well.

--- data/base/data-updates.lua
+++ data/base/data-updates.lua
@@ -1,9 +1,6 @@
 require("scripts.icons")
 require("scripts.fluids")
-require("scripts.updates")
 require("scripts.utils")
-
-local prototypes = require("data.base.prototypes")
 
 local setting = config("filled-barrels")
 if setting ~= Options.none then
@@ -14,11 +11,11 @@ if setting ~= Options.none then
         if setting == "fluid-icon" then
           overlay_icon(barrel, get_and_shift_fluid_icons(name))
         else
-          local proto = prototypes[name]
-          if proto and proto.type == "fluid" then
-            local setting = config(name)
-            if setting ~= Options.icon then
-              do_replace_or_overlay(name .. "-barrel", table_merge(proto, { type = "item" }), name)
+          local icons = icons_from_obj(fluid)
+          if #icons > 1 then
+            table.remove(icons, 1)
+            for _, icon in pairs(icons) do
+              table.insert(barrel.icons, icon)
             end
           end
         end
