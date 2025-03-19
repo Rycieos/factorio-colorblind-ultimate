Rycieos/factorio-colorblind-ultimate

commit c3065ec41b5796c6604d85e56e8d4f309deac7eb

Fix offsets of tier icons on large entities
This is mostly caused by the drop of the non-HR sprites. But also making
the tier icons have so much dead space was not helping either.

I am not sure why the offsets without the negative worked before, but it
gives better results now than without it.

scripts/sprites.lua
@@ -9,8 +9,8 @@ local function shift_overlay_sprite(base_sprite, target_sprite)
   end
 
   local shift = {
-    base_sprite.shift[1] + target_sprite.shift[1] * (base_sprite.width or base_sprite.size) / 32,
-    base_sprite.shift[2] + target_sprite.shift[2] * (base_sprite.height or base_sprite.size) / 32,
+    -(base_sprite.shift[1] or 0) + target_sprite.shift[1] * (base_sprite.width or base_sprite.size) / 64,
+    -(base_sprite.shift[2] or 0) + target_sprite.shift[2] * (base_sprite.height or base_sprite.size) / 64,
   }
   return table_merge(target_sprite, { shift = shift })
 end

scripts/icon_overlays.lua
@@ -17,6 +17,12 @@ BaseOverlays = {
     scale = IconScale * 0.4,
     shift = primary_shift,
   },
+  small_constant = {
+    icon_size = 64,
+    icon_mipmaps = 3,
+    scale = IconScale * 0.4,
+    shift = primary_shift,
+  },
   tooltip = {
     icon_size = 40,
     icon_mipmaps = 2,
@@ -66,6 +72,12 @@ local function create_constant(name, path)
   })
 end
 
+local function create_small_constant(name, path)
+  create_overlay(name, BaseOverlays.small_constant, {
+    icon = path,
+  })
+end
+
 local function create_tooltip(name, path)
   create_overlay(name, BaseOverlays.tooltip, {
     icon = path and path or IconPath .. "tooltips/" .. name .. ".png",
@@ -93,10 +105,10 @@ create_constant("efficiency", CoreIconPath .. "technology/constants/constant-bat
 create_constant("productivity", CoreIconPath .. "technology/constants/constant-mining-productivity.png")
 create_constant("speed", CoreIconPath .. "technology/constants/constant-speed.png")
 create_constant("quality", CoreIconPath .. "technology/constants/constant-count.png")
-create_constant("tier-1", IconPath .. "constants/tier-1.png")
-create_constant("tier-2", IconPath .. "constants/tier-2.png")
-create_constant("tier-3", IconPath .. "constants/tier-3.png")
-create_constant("tier-4", IconPath .. "constants/tier-4.png")
+create_small_constant("tier-1", IconPath .. "constants/tier-1.png")
+create_small_constant("tier-2", IconPath .. "constants/tier-2.png")
+create_small_constant("tier-3", IconPath .. "constants/tier-3.png")
+create_small_constant("tier-4", IconPath .. "constants/tier-4.png")
 
 create_tooltip("buffer")
 create_tooltip("consumes")

commit 9bda16edcf1332e3bc1d7ff32d85e9a3af22fb23

Fix wrong position for train stop no-entry lights
I broke this in commit c3065ec41b5796c6604d85e56e8d4f309deac7eb. Clearly
I missed looking at the train stops, because this was very very wrong.

I commented that the change made it appear better, but now I don't see
much of a difference, except on the train stop.

scripts/sprites.lua
@@ -9,8 +9,8 @@ local function shift_overlay_sprite(base_sprite, target_sprite)
   end
 
   local shift = {
-    -(base_sprite.shift[1] or 0) + target_sprite.shift[1] * (base_sprite.width or base_sprite.size) / 64,
-    -(base_sprite.shift[2] or 0) + target_sprite.shift[2] * (base_sprite.height or base_sprite.size) / 64,
+    (base_sprite.shift[1] or 0) + target_sprite.shift[1] * (base_sprite.width or base_sprite.size) / 64,
+    (base_sprite.shift[2] or 0) + target_sprite.shift[2] * (base_sprite.height or base_sprite.size) / 64,
   }
   return table_merge(target_sprite, { shift = shift })
 end